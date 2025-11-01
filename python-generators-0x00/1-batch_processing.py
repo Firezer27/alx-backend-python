380d7dfb0fd7825d3c3124b5142415450459c0c8
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-01 16:15:44 +0300

    add some python file

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            database='ALX_prodev'
+        )
+
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data;")
+
+        while True:
+            batch = cursor.fetchmany(batch_size)
+            if not batch:
+                break
+            yield batch
+
+    except mysql.connector.Error as err:
+        print(f"Database error: {err}")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
